e4ef96360bc562deb0287c9350b286a60f646ae9
Martin Loic <loic.martin2000@gmail.com>
2024-07-16 09:13:06 +0200

    variables types

---
+++ src/project/00-variables/variable_types.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Jean";
+$age = 39;
+$eyes_colour = "blue";
+$family_name = array("Jean", "Jeanne");
+$is_hungry = true;
+
+echo "<p>Hi! My name is $name</p>";
+echo "<p>I am $age years old.</p>";
+echo "<p>My eyes are $eyes_colour</p>";
+echo "<p>The first person in my family is $family_name[1]</p>";
